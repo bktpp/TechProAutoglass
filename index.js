c3ebf1e5ab00a51efe1d81e5fac4304ca740d9e7
bktpp <117415722+bktpp@users.noreply.github.com>
2023-12-07 13:03:01 -0600

    added email form functionality for users

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ const allSections = document.querySelectorAll(".section");
 
 const revealSection = function (entries, observer) {
    const [entry] = entries;
-   console.log(entry);
 
    if (!entry.isIntersecting) return;
    entry.target.classList.remove("section--hidden");
@@ -37,3 +36,5 @@ allSections.forEach(function (section) {
    sectionObserver.observe(section);
    section.classList.add("section--hidden");
 });
+
+/*---------Email.js Form------------*/
